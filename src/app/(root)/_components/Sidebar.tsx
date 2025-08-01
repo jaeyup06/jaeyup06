5c90ee133e99e9bbf93607e3d5b65c887f7e1b5e
재엽 <jaeyup06@gmail.com>
2025-08-02 07:38:12 +0900

    chore: 사이드바 펼쳤을 때, 요소만 먼저 보이는 상황 수정

--- src/app/(root)/_components/Sidebar.tsx
+++ src/app/(root)/_components/Sidebar.tsx
@@ -62,177 +62,183 @@ function Sidebar() {
       ? "font-bold text-white bg-gray-900 rounded px-1"
       : "mx-1 hover:text-gray-400 bg-transparent border-none";
 
-  // 사이드바 자세히
-  if (isOpen) {
-    return (
-      <div className="grid h-screen w-[250px] bg-gray-800 text-white transition-all duration-300">
-        <div className="flex flex-col items-center py-10 px-[27px]">
-          {/* 토글 */}
-          <button
-            onClick={toggleSidebar}
-            className="absolute top-4 left-4 text-2xl hover:text-gray-400"
-            aria-label="사이드바 간략히"
-          >
-            <FaBars />
-          </button>
+  return (
+    <div
+      className={`overflow-hidden transition-all duration-300 bg-gray-800 text-white h-screen flex ${
+        isOpen ? "w-[250px]" : "w-[58px]"
+      }`}
+    >
+      {isOpen ? (
+        // 사이드바 자세히
+        <div className="grid w-full">
+          <div className="flex flex-col items-center py-10 px-[27px]">
+            {/* 토글 */}
+            <button
+              onClick={toggleSidebar}
+              className="absolute top-4 left-4 text-2xl hover:text-gray-400"
+              aria-label="사이드바 간략히"
+            >
+              <FaBars />
+            </button>
 
-          {/* 정보 */}
-          <img
-            src="/profile.jpg"
-            className="w-[104px] h-[104px] rounded-full border-[3px] border-white"
-            alt="프로필"
-          />
-          <h1 className="text-xl font-bold mt-4">김재엽</h1>
-          <ul className="space-y-1 mt-2 text-gray-200 text-sm text-center whitespace-nowrap">
-            <li>한양대학교 ERICA · Robotics 전공</li>
-            <li>010-0000-0000</li>
-            <li>jaeyup06@hanyang.ac.kr</li>
-          </ul>
+            {/* 정보 */}
+            <img
+              src="/profile.jpg"
+              className="w-[104px] h-[104px] rounded-full border-[3px] border-white"
+              alt="프로필"
+            />
+            <h1 className="text-xl font-bold mt-4">김재엽</h1>
+            <ul className="space-y-1 mt-2 text-gray-200 text-sm text-center whitespace-nowrap">
+              <li>한양대학교 ERICA · Robotics 전공</li>
+              <li>010-0000-0000</li>
+              <li>jaeyup06@hanyang.ac.kr</li>
+            </ul>
 
-          {/* 메뉴 */}
-          <div className="flex-grow flex items-center w-28">
-            <ul className="text-xl text-white space-y-4">
-              {menuItems.map(({ label, icon: Icon, children }) => (
-                <div key={label}>
-                  <li className="flex items-center gap-2">
-                    <Icon />
-                    {label}
-                  </li>
+            {/* 메뉴 */}
+            <div className="flex-grow flex items-center w-28">
+              <ul className="text-xl text-white space-y-4">
+                {menuItems.map(({ label, icon: Icon, children }) => (
+                  <div key={label}>
+                    <li className="flex items-center gap-2">
+                      <Icon />
+                      {label}
+                    </li>
+                    {/* 하위 메뉴 */}
+                    <ul className="text-sm space-y-1 text-gray-200 pl-6">
+                      {children.map(({ label: childLabel, path }) => (
+                        <li key={path}>
+                          <button
+                            onClick={() => goToPath(path)}
+                            className={getMenuClass(path)}
+                          >
+                            {childLabel}
+                          </button>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                ))}
+              </ul>
+            </div>
+
+            {/* SNS */}
+            <div className="flex gap-8 text-white text-4xl items-center">
+              <a
+                href="https://github.com/jaeyup06"
+                target="_blank"
+                aria-label="GitHub"
+                className="hover:text-gray-400"
+              >
+                <FaGithub />
+              </a>
+              <a
+                href="https://instagram.com/jaeyup06"
+                target="_blank"
+                aria-label="Instagram"
+                className="hover:text-gray-400"
+              >
+                <FaInstagram />
+              </a>
+              <a
+                href="https://youtube.com/@jaeyup06"
+                target="_blank"
+                aria-label="YouTube"
+                className="hover:text-gray-400"
+              >
+                <FaYoutube />
+              </a>
+            </div>
+          </div>
+        </div>
+      ) : (
+        // 사이드바 간략히
+        <div className="flex flex-col items-center w-full">
+          <div className="flex flex-col justify-between items-center h-full w-full pt-16 pb-8">
+            {/* 토글 */}
+            <button
+              onClick={toggleSidebar}
+              className="absolute top-4 left-4 text-2xl hover:text-gray-400"
+              aria-label="사이드바 자세히"
+            >
+              <FaBars />
+            </button>
+
+            {/* 정보 */}
+            <img
+              src="/profile.jpg"
+              className="w-[36px] h-[36px] rounded-full border-[2px] border-white"
+              alt="프로필"
+            />
+
+            {/* 메뉴 */}
+            <div className="flex flex-col gap-10 items-center relative">
+              {menuItems.map(({ icon: Icon, label, children }) => (
+                <div
+                  key={label}
+                  className="relative"
+                  onMouseEnter={() => setOpenMenu(label)}
+                  onMouseLeave={() => setOpenMenu(null)}
+                >
+                  <div className="absolute left-0 top-0 h-full w-[30px]" />
+
+                  <button
+                    className="text-2xl relative leading-none flex hover:text-gray-400"
+                    aria-label={label}
+                  >
+                    <Icon className="transition-colors duration-200" />
+                  </button>
+
                   {/* 하위 메뉴 */}
-                  <ul className="text-sm space-y-1 text-gray-200 pl-6">
-                    {children.map(({ label: childLabel, path }) => (
-                      <li key={path}>
-                        <button
-                          onClick={() => goToPath(path)}
-                          className={getMenuClass(path)}
-                        >
-                          {childLabel}
-                        </button>
-                      </li>
-                    ))}
-                  </ul>
+                  {openMenu === label && (
+                    <div className="absolute left-[30px] top-1/2 -translate-y-1/2 bg-gray-700 p-2 rounded shadow-md flex flex-col gap-1 z-50 text-sm text-white whitespace-nowrap">
+                      {children.map(({ label: childLabel, path }) => (
+                        <span key={path}>
+                          <button
+                            onClick={() => goToPath(path)}
+                            className={`${getMenuClass(path)} text-left`}
+                          >
+                            {childLabel}
+                          </button>
+                        </span>
+                      ))}
+                    </div>
+                  )}
                 </div>
               ))}
-            </ul>
-          </div>
+            </div>
 
-          {/* SNS */}
-          <div className="flex gap-8 text-white text-4xl items-center">
-            <a
-              href="https://github.com/jaeyup06"
-              target="_blank"
-              aria-label="GitHub"
-              className="hover:text-gray-400"
-            >
-              <FaGithub />
-            </a>
-            <a
-              href="https://instagram.com/jaeyup06"
-              target="_blank"
-              aria-label="Instagram"
-              className="hover:text-gray-400"
-            >
-              <FaInstagram />
-            </a>
-            <a
-              href="https://youtube.com/@jaeyup06"
-              target="_blank"
-              aria-label="YouTube"
-              className="hover:text-gray-400"
-            >
-              <FaYoutube />
-            </a>
-          </div>
-        </div>
-      </div>
-    );
-  } else {
-    // 사이드바 간략히
-    return (
-      <div className="h-screen w-[58px] bg-gray-800 text-white flex flex-col items-center transition-all duration-300">
-        <div className="flex flex-col justify-between items-center h-full w-full pt-16 pb-8">
-          {/* 토글 */}
-          <button
-            onClick={toggleSidebar}
-            className="absolute top-4 left-4 text-2xl hover:text-gray-400"
-            aria-label="사이드바 자세히"
-          >
-            <FaBars />
-          </button>
-          {/* 정보 */}
-          <img
-            src="/profile.jpg"
-            className="w-[36px] h-[36px] rounded-full border-[2px] border-white"
-            alt="프로필"
-          />
-
-          {/* 메뉴 */}
-          <div className="flex flex-col gap-10 items-center relative">
-            {menuItems.map(({ icon: Icon, label, children }) => (
-              <div
-                key={label}
-                className="relative"
-                onMouseEnter={() => setOpenMenu(label)}
-                onMouseLeave={() => setOpenMenu(null)}
+            {/* SNS */}
+            <div className="flex flex-col gap-10 items-center text-2xl">
+              <a
+                href="https://github.com/jaeyup06"
+                target="_blank"
+                aria-label="GitHub"
+                className="hover:text-gray-400"
               >
-                <div className="absolute left-0 top-0 h-full w-[30px]" />
-
-                <button
-                  className="text-2xl relative leading-none flex hover:text-gray-400"
-                  aria-label={label}
-                >
-                  <Icon className="transition-colors duration-200" />
-                </button>
-
-                {/* 하위 메뉴 */}
-                {openMenu === label && (
-                  <div className="absolute left-[30px] top-1/2 -translate-y-1/2 bg-gray-700 p-2 rounded shadow-md flex flex-col gap-1 z-50 text-sm text-white whitespace-nowrap">
-                    {children.map(({ label: childLabel, path }) => (
-                      <span key={path}>
-                        <button
-                          onClick={() => goToPath(path)}
-                          className={`${getMenuClass(path)} text-left`}
-                        >
-                          {childLabel}
-                        </button>
-                      </span>
-                    ))}
-                  </div>
-                )}
-              </div>
-            ))}
-          </div>
-          {/* SNS */}
-          <div className="flex flex-col gap-10 items-center text-2xl">
-            <a
-              href="https://github.com/jaeyup06"
-              target="_blank"
-              aria-label="GitHub"
-              className="hover:text-gray-400"
-            >
-              <FaGithub />
-            </a>
-            <a
-              href="https://instagram.com/jaeyup06"
-              target="_blank"
-              aria-label="Instagram"
-              className="hover:text-gray-400"
-            >
-              <FaInstagram />
-            </a>
-            <a
-              href="https://youtube.com/@jaeyup06"
-              target="_blank"
-              aria-label="YouTube"
-              className="hover:text-gray-400"
-            >
-              <FaYoutube />
-            </a>
+                <FaGithub />
+              </a>
+              <a
+                href="https://instagram.com/jaeyup06"
+                target="_blank"
+                aria-label="Instagram"
+                className="hover:text-gray-400"
+              >
+                <FaInstagram />
+              </a>
+              <a
+                href="https://youtube.com/@jaeyup06"
+                target="_blank"
+                aria-label="YouTube"
+                className="hover:text-gray-400"
+              >
+                <FaYoutube />
+              </a>
+            </div>
           </div>
         </div>
-      </div>
-    );
-  }
+      )}
+    </div>
+  );
 }
 
 export default Sidebar;
