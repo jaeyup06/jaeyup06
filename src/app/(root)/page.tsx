025f454aa5286db59d493f833a83ad8455645798
jaeyup06 <jaeyup06@gmail.com>
2025-10-06 03:59:06 +0900

    style: 모바일 미디어 쿼리 스케일을 뷰포트 기준으로 변경

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -5,7 +5,7 @@ function Home() {
     <main className="flex items-center justify-center min-h-screen bg-[url('/banner.jpg')] bg-cover bg-center h-screen w-screen overflow-hidden">
       <div
         className="backdrop-blur-2xl bg-white/10 border border-white/20 items-center py-16 px-16 rounded-3xl shadow-xl flex flex-col lg:flex-row lg:space-x-16 text-gray-100
-                   max-sm:scale-[0.75]"
+                   max-sm:scale-[calc(2vw/11px)]"
       >
         <img
           src="/profile.jpg"
